3e82cf3b73b18347f96dfe68a961d8ac0445d30b
Pons Michaël <mikpons2@gmail.com>
2022-06-13 12:44:07 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ChFbuHBnZaOOfT_sAypkAVVlBWuwN2UMq_jQRyveqd4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyB1C6QeMO-1uq5sV6kp_7lCYOr9aGETaEw';
+   // 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
